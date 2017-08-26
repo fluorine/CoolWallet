6f946727c9648c0ee95802b10e7518df0f9dca24
fluorine <josephmail7@gmail.com>
2017-08-25 23:20:44 -0400

    Cool Wallet Core

---
+++ CoolWallet.Core/CoolWallet/IWalletPart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolWallet.Core
+{
+    public interface IWalletPart : IShortNotationFormat, IValidable, IEquatable<IWalletPart>
+    {
+        IWalletSignature Signature { get; set; }
+
+        string Data { get; set; }
+    }
+}
---
+++ CoolWallet.Core/CoolWallet/IWalletSignature.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolWallet.Core
+{
+    public interface IWalletSignature : IShortNotationFormat, IValidable, IEquatable<IWalletSignature>
+    {
+        /// <summary>
+        /// Version used to generate the parts of the Cold Wallet.
+        /// </summary>
+        int Version { get; }
+
+        /// <summary>
+        /// Numbers of parts required to recompose a Wallet (Private Key).
+        /// It is a numerator.
+        /// </summary>
+        int PartsThreshold { get; }
+
+        /// <summary>
+        /// Total of parts generated from the Wallet (Privete Key).
+        /// It is a denominator.
+        /// </summary>
+        int PartsTotal { get; }
+    }
+}
